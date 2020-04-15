8ebdb499cbcf9bb809ef5ccc07b6b2a7b37a69d7
KMRH47 <kramze47@gmailcom>
2020-04-15 13:20:22 +0200

    Add project files.

---
+++ EncrypterDecrypter/Encrypter.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EncrypterDecrypter
+{
+    class Encrypter
+    {
+        public static string Encrypt(string text)
+        {
+            string textEncrypted = "";
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                int letterInt = text[i] + 1;
+                char letter = (char)letterInt;
+                textEncrypted += letter.ToString();
+            }
+
+            return textEncrypted;
+        }
+
+        public static string Decrypt(string text)
+        {
+            string textDecrypted = "";
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                char letter = (char)(text[i] - 1);
+                textDecrypted += letter.ToString();
+            }
+
+            return textDecrypted;
+        }
+    }
+}
---
+++ EncrypterDecrypter/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace EncrypterDecrypter
+{
+    class Program
+    {
+        static void Main()
+        {
+            bool debug = true;
+
+            string newText = Encrypter.Encrypt("He");
+            Console.WriteLine("Text encripted: {0}", newText);
+            string TextDescripted = Encrypter.Decrypt(newText);
+            Console.WriteLine("Text Decripted: {0}", TextDescripted);
+
+            if (debug)
+                Console.ReadLine();
+        }
+    }
+}
